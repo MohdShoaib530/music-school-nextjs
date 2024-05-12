8ea09bc48e3b94cb1911850cc61ad213c625a2a5
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-05-12 23:20:29 +0530

    Add UpcomingWebinars component to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import FeaturedCourses from "@/components/FeaturedCourses";
-import HeroSection from "@/components/HeroSection";
-import WhyChooseUs from "@/components/WhyChooseUs";
-import TestimonialCards from "@/components/TestimonialCards";
+import FeaturedCourses from '@/components/FeaturedCourses';
+import HeroSection from '@/components/HeroSection';
+import WhyChooseUs from '@/components/WhyChooseUs';
+import TestimonialCards from '@/components/TestimonialCards';
+import UpcomingWebinars from '../components/UpcomingWebinars';
 
 export default function Home() {
 	return (
-		<main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+		<main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
 			<HeroSection />
 			<FeaturedCourses />
 			<WhyChooseUs />
 			<TestimonialCards />
+			<UpcomingWebinars />
 		</main>
 	);
 }
